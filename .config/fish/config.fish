7ca83afa8c2bb5b0cbda9a464e321c6c4a4cfde5
scrimblo <beebroox7@zohomail.com>
2022-10-05 20:03:15 -0600

    color scheme updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -42,7 +42,7 @@ end
 
 
 ## Advanced command-not-found hook
-source /usr/share/doc/find-the-command/ftc.fish
+# source /usr/share/doc/find-the-command/ftc.fish
 
 
 ## Functions
@@ -102,57 +102,57 @@ alias ls='ls -A --color=always --group-directories-first' # preferred listing
 # alias ll='exa -l --color=always --group-directories-first --icons'  # long format
 # alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
 # alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
-alias ip="ip -color"
+# alias ip="ip -color"
 
 # Replace some more things with better alternatives
-alias cat='bat --style header --style rules --style snip --style changes --style header'
-[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
-alias vim='nvim'
+# alias cat='bat --style header --style rules --style snip --style changes --style header'
+# [ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
+# alias vim='nvim'
 
 # Common use
-alias grubup="sudo update-grub"
-alias fixpacman="sudo rm /var/lib/pacman/db.lck"
-alias tarnow='tar -acf '
-alias untar='tar -xvf '
-alias wget='wget -c '
-alias rmpkg="sudo pacman -Rdd"
-alias psmem='ps auxf | sort -nr -k 4'
-alias psmem10='ps auxf | sort -nr -k 4 | head -10'
-alias upd='/usr/bin/garuda-update'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias hw='hwinfo --short'                          # Hardware Info
-alias big="expac -H M '%m\t%n' | sort -h | nl"     # Sort installed packages according to size in MB
-alias gitpkg='pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
+# alias grubup="sudo update-grub"
+# alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+# alias tarnow='tar -acf '
+# alias untar='tar -xvf '
+# alias wget='wget -c '
+# alias rmpkg="sudo pacman -Rdd"
+# alias psmem='ps auxf | sort -nr -k 4'
+# alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+# alias upd='/usr/bin/garuda-update'
+# alias ..='cd ..'
+# alias ...='cd ../..'
+# alias ....='cd ../../..'
+# alias .....='cd ../../../..'
+# alias ......='cd ../../../../..'
+# alias dir='dir --color=auto'
+# alias vdir='vdir --color=auto'
+# alias grep='grep --color=auto'
+# alias fgrep='fgrep --color=auto'
+# alias egrep='egrep --color=auto'
+# alias hw='hwinfo --short'                          # Hardware Info
+# alias big="expac -H M '%m\t%n' | sort -h | nl"     # Sort installed packages according to size in MB
+# alias gitpkg='pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
 
-# Get fastest mirrors
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
-alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
-alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
-alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+# # Get fastest mirrors
+# alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+# alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+# alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+# alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
 
-# Help people new to Arch
-alias apt='man pacman'
-alias apt-get='man pacman'
-alias please='sudo'
-alias tb='nc termbin.com 9999'
+# # Help people new to Arch
+# alias apt='man pacman'
+# alias apt-get='man pacman'
+# alias please='sudo'
+# alias tb='nc termbin.com 9999'
 
-# Cleanup orphaned packages
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+# # Cleanup orphaned packages
+# alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
 
-# Get the error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
+# # Get the error messages from journalctl
+# alias jctl="journalctl -p 3 -xb"
 
-# Recent installed packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+# # Recent installed packages
+# alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
 /bin/echo -e "
   \e]P0342828
@@ -176,6 +176,6 @@ alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 clear
 
 ## Run fastfetch if session is interactive
-if status --is-interactive && type -q fastfetch
-   fastfetch --load-config neofetch
-end
+#if status --is-interactive && type -q fastfetch
+#   fastfetch --load-config neofetch
+#end
